89d45cb5b1a622562c156cab2d51e3e76d99ace4
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2022-01-14 23:06:41 +0100

    Add NavLinks to Navbar

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import Button from '../components/Button'
+import { NavLink } from "react-router-dom"
 
 const Navbar = (props) => {
     const { handleAuth } = props
     return (
         <nav>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/post">Post</a></li>
-                <li><a href="/signin">SignIn</a></li>
-                <li><a href="/signup">SignUp</a></li>
-                <li><a href="/thread">Thread</a></li>
-                <li><a href="/user">User</a></li>
+                <li><NavLink to='/'>Home</NavLink></li>
+                <li><NavLink to='/about'>About</NavLink></li>
+                <li><NavLink to='/post'>Post</NavLink></li>
+                <li><NavLink to='/signin'>SignIn</NavLink></li>
+                <li><NavLink to='/signup'>SignUp</NavLink></li>
+                <li><NavLink to='/thread'>Thread</NavLink></li>
+                <li><NavLink to='/user'>User</NavLink></li>
             </ul>
             <Button handleMethod={() => handleAuth(false)}>Sing Out</Button>
         </nav>
