a6e424469ae09afe38b49f3aa78675c1ec09c029
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2022-01-15 16:33:14 +0100

    Add navbar signin and navbar signout

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -4,22 +4,43 @@ import { NavLink } from "react-router-dom"
 import { removeData } from '../utils/dataUtils'
 
 const Navbar = (props) => {
+    const { isAuth, handleAuth } = props
+    return (
+        <>
+            {isAuth ? <NavbarSignedIn handleAuth={handleAuth} /> : <NavbarSignedOut />}
+        </>
+    )
+}
+
+const NavbarSignedIn = (props) => {
     const { handleAuth } = props
+
     return (
         <nav>
             <ul>
                 <li><NavLink to='/'>Home</NavLink></li>
                 <li><NavLink to='/about'>About</NavLink></li>
-                <li><NavLink to='/post'>Post</NavLink></li>
-                <li><NavLink to='/signin'>SignIn</NavLink></li>
-                <li><NavLink to='/signup'>SignUp</NavLink></li>
                 <li><NavLink to='/thread'>Thread</NavLink></li>
                 <li><NavLink to='/user'>User</NavLink></li>
             </ul>
             <Button handleMethod={() => {
                 handleAuth(false)
                 removeData('loggedUserId')
-            }}>Sing Out</Button>
+            }}>Sign Out</Button>
+        </nav>
+    )
+}
+
+const NavbarSignedOut = () => {
+    return (
+        <nav>
+            <ul>
+                <li><NavLink to='/'>Home</NavLink></li>
+                <li><NavLink to='/about'>About</NavLink></li>
+                <li><NavLink to='/signin'>SignIn</NavLink></li>
+                <li><NavLink to='/signup'>SignUp</NavLink></li>
+                <li><NavLink to='/thread'>Thread</NavLink></li>
+            </ul>
         </nav>
     )
 }
