187520d97ee9697db75b7253f3529459ff2536d2
Lukasz Smolnicki <lukasz.smolnicki@gmail.com>
2022-02-28 23:53:05 +0100

    Add footer

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
     return (
-        <footer className='navbar navbar-expand-sm fixed-bottom navbar-light bg-danger'>
-            <div className='container'>
-                text
+        <footer class="bg-danger fixed-bottom text-center text-lg-start">
+            <div class="text-center p-3">
+                © 2022 Created by: <a class="link-dark text-decoration-none" href="https://github.com/lukasz-smolnicki"><FontAwesomeIcon icon={faGithub} /> Łukasz Smolnicki</a>
             </div>
         </footer>
     )
